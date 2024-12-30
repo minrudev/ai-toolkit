dc95e10ba9acefe2af45e5eace5fe832b058cd14
mrudzevicius <mindaugaseth@gmail.com>
2024-12-30 17:27:48 +0200

    Add 24GB training file.

--- jobs/MergeJob.py
+++ jobs/MergeJob.py
@@ -3,18 +3,17 @@ from collections import OrderedDict
 from jobs import BaseJob
 from toolkit.train_tools import get_torch_dtype
 
-process_dict = {
-}
+process_dict = {}
 
 
 class MergeJob(BaseJob):
 
     def __init__(self, config: OrderedDict):
         super().__init__(config)
-        self.dtype = self.get_conf('dtype', 'fp16')
+        self.dtype = self.get_conf("dtype", "fp16")
         self.torch_dtype = get_torch_dtype(self.dtype)
-        self.is_v2 = self.get_conf('is_v2', False)
-        self.device = self.get_conf('device', 'cpu')
+        self.is_v2 = self.get_conf("is_v2", False)
+        self.device = self.get_conf("device", "cpu")
 
         # loads the processes from the config
         self.load_processes(process_dict)
@@ -23,7 +22,9 @@ class MergeJob(BaseJob):
         super().run()
 
         print("")
-        print(f"Running  {len(self.process)} process{'' if len(self.process) == 1 else 'es'}")
+        print(
+            f"Running  {len(self.process)} process{'' if len(self.process) == 1 else 'es'}"
+        )
 
         for process in self.process:
             process.run()
